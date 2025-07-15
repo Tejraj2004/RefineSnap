05a3aff9d157de7fb5df8f5488373ddd070d0d20
Tejraj Sahu <tejrajsahu968@gmail.com>
2025-07-15 19:28:09 +0530

    Webhooks Added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import 'dotenv/config' //to get all env variables
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
+import userRouter from './routes/useRoutes.js'
+
 //App Config
 const PORT = process.env.PORT || 4000
 const app = express()
@@ -14,6 +16,7 @@ app.use(cors())// cors helps us to connect our client running on differnt port
 
 //API routes
 app.get('/',(req,res)=>res.send("API Working"))
+app.use('/api/user',userRouter)
 
 app.listen(PORT, ()=> console.log("Server Running on port "+PORT))
  
